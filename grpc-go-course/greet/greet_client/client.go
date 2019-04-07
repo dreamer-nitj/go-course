a4d52447c3e1dc29fec37f98ad35a24f7a0f23f0
Ankit Kumar <ankitbansal.nitj@gmail.com>
2019-04-07 16:22:51 +0530

    fully functional client/server with simple Greet API

--- grpc-go-course/greet/greet_client/client.go
+++ grpc-go-course/greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
@@ -20,4 +21,22 @@ func main() {
 
 	c := greetpb.NewGreetServiceClient(conn)
 	fmt.Printf("Created client: %f", c)
+
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a Unary RPC...")
+	request := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Ankit",
+			LastName:  "Kumar",
+		},
+	}
+	resp, err := c.Greet(context.Background(), request)
+	if err != nil {
+		log.Fatalf("Error in invoking Greet method from client: %v", err)
+	}
+
+	log.Printf("Response from greet: %v", resp.Result)
 }
--- grpc-go-course/greet/greet_server/server.go
+++ grpc-go-course/greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -10,6 +11,18 @@ import (
 )
 
 type server struct {
+}
+
+func (s *server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse,
+	error) {
+	fmt.Printf("Greet function was invoked with %v\n", req)
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+
+	return res, nil
 }
 
 func main() {
